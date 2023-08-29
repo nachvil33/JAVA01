2871fb1110eddd4440da9fd0127aa071b584623e
nachvil33 <mokacinez39@gmail.com>
2023-08-29 10:36:28 -0600

    Final changes, correction of several issues, like problems whit the use of exponent after several operations, and the fullfilment of the jest test, and rigorous test whit dev tools using chrome

--- 08_calculator/calculator-events.js
+++ 08_calculator/calculator-events.js
@@ -1,5 +1,6 @@
 const display = document.getElementById('display');
 let currentInput = '';
+let pendingExponent = false;
 
 document.querySelectorAll('.btn').forEach(button => {
   button.addEventListener('click', () => handleButtonClick(button.innerText));
@@ -9,15 +10,29 @@ function handleButtonClick(value) {
   switch (value) {
     case 'C':
       currentInput = '';
+      pendingExponent = false;
       break;
     case '←':
       currentInput = currentInput.slice(0, -1);
       break;
     case '=':
       try {
-        currentInput = eval(currentInput).toString();
+        if (pendingExponent) {
+          // Si estamos en el modo de ingreso de exponente, calculamos la potencia
+          const [base, exponent] = currentInput.split('**');
+          currentInput = Math.pow(parseFloat(base), parseFloat(exponent)).toString();
+          pendingExponent = false; // Restablecemos pendingExponent
+        } else {
+          currentInput = eval(currentInput).toString();
+        }
       } catch (error) {
         currentInput = 'Error';
+      }
+      break;
+    case 'x^y':
+      if (!pendingExponent) {
+        currentInput += '**';
+        pendingExponent = true;
       }
       break;
     default:
